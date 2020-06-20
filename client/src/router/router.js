fd9c6c64183fa78c38a4e31b589ff21c9ebf1600
Marwen Magri <marwen.magri@gomycode.co>
2020-06-20 13:25:34 +0000

    testing wave effect

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import NavBar from "../components/views/Navigation/NavContainer"
-
+import Home from "../components/views/home/Home"
 const AppRouter = () => (
 <Router>
 
     <NavBar />
     <Switch>
-
+    <Route path="/" exact component={Home} />
     </Switch>
 </Router>
 )
