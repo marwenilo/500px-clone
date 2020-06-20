973b38f30408e77520abf302144b34c4e66f2054
Marwen Magri <marwen.magri@gomycode.co>
2020-06-20 12:29:10 +0000

    Nav Container

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
-import NavBar from "../components/views/Navigation/NavBar"
+import NavBar from "../components/views/Navigation/NavContainer"
 
 const AppRouter = () => (
 <Router>
