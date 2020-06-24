7f8ac2485a84725993120b4b1b63290c49e80139
Marwen Magri <marwen.magri@gomycode.co>
2020-06-24 16:35:17 +0000

    editor choice bottom part

--- client/src/components/views/home/EditorChoice.js
+++ client/src/components/views/home/EditorChoice.js
@@ -4,7 +4,10 @@ import imh from "../../../assets/images/1.jpg";
 
 
 const styles = (theme) => ({
-  editorContainer: {
+  editorContainer:{
+
+  },
+  editorHeaderContainer: {
     height:"500px",
     wordBreak: "break-word",
     maxWidth: "100%",
@@ -20,8 +23,8 @@ const styles = (theme) => ({
 
   },
   dote:{
-    height: "200px",
-    width: "200px",
+    height: "150px",
+    width: "150px",
     backgroundColor: "rgb(12, 239, 232)",
     borderRadius: "50%",
     display: "flex",
@@ -36,16 +39,70 @@ const styles = (theme) => ({
     textTransform: "none",
     color: "rgb(255, 255, 255)",
     margin: "0px",
-  }
+  },
+  EditorBtn:{
+    display: "inline-flex",
+    justifyContent: "center",
+    alignItems: "center",
+    fontWeight: "bold",
+    width: "19%",
+    height: "80px",
+    minWidth: "216px",
+    lineHeight: "56px",
+    fontSize: "30px",
+    backgroundColor: "rgb(255, 255, 255)",
+    color: "white",
+    backgroundColor:"black",
+    padding: "0px 31px",
+    borderRadius: "100px",
+    margin: "16px 0px 0px",
+    borderWidth: "initial",
+    borderStyle: "none",
+    borderColor: "initial",
+    borderImage: "initial",
+    transition: "background-color 0.1s ease 0s",
+    textDecoration: "none",
+  },
+  editorBottomContainer:{
+    padding:"32px 64px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "228px",
+},
+pEditor:{
+
+  fontSize: "20px",
+    lineHeight: "20px",
+    letterSpacing: "0px",
+    textTransform: "none",
+    color: "rgb(34, 34, 34)",
+}
+
 });
 function EditorChoise(props) {
   const { classes } = props;
 
-  return <div className={classes.editorContainer}>
+  return(
+    <div className={classes.editorContainer}>
+     <div className={classes.editorHeaderContainer}>
 <span className={classes.dote}>
-<i class="fas fa-award fa-7x"></i>
+<i class="fas fa-award fa-6x"></i>
 </span>
 <h2 className={classes.hEditor}> <span>Editors’ Choice</span></h2>
-  </div>;
+</div>
+<div className={classes.editorBottomContainer}>
+  <p className={classes.pEditor}>
+    <span>
+    Discover fresh inspiration daily. Your photos could even be featured in our handpicked Editors’ Choice Gallery.
+    </span>
+    </p>
+    <a variant="outlined" size="large" color="primary" href="login" className={classes.EditorBtn} >
+   View Editors’ Choice
+        </a>
+  
+</div>
+
+  </div>);
 }
 export default withStyles(styles, { withTheme: true })(EditorChoise);
